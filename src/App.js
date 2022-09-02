3a984fe7fb8622a46b704af114daf2b1c586e1a7
Tkachenko Yevhen <zekatka01@gmail.com>
2022-09-02 17:39:58 +0200

    add scss + categor header pizblock sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
+import Header from "./Components/Header";
+import Categories from "./Components/Categories";
+import Sort from "./Components/Sort";
+import PizzaBlock from "./Components/PizzaBlock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price="500" />
+            <PizzaBlock title="Маргарита" price="4 00" />
+            <PizzaBlock title="4 сыра" price="600" />
+            <PizzaBlock title="Куриная" price="900" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
